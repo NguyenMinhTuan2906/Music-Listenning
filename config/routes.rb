d8d84fbec5f10fc6b64b4088bcbad3c78d37821b
NguyenMinhTuan2906 <monteeladykillah@gmail.com>
2017-06-29 16:08:05 +0700

    Admin Interface

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users
+
+  namespace :admin do
+    root "homes#index"
+    resources :users, only: [:show, :index, :destroy]
+  end
 end
