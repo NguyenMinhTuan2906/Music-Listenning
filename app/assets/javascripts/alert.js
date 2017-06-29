34c5986a23b7d685c059d31beea18ac428925369
NguyenMinhTuan2906 <monteeladykillah@gmail.com>
2017-06-29 08:21:38 +0700

    User login logout

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   setTimeout(function(){
     $('.set-timeout').slideUp();
   },2000);
+  setTimeout(function(){
+    $('.error-explanation').fadeOut();
+  },6000);
 });
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      flash[:success] = t(".welcome") + user.name + "."
+      redirect_to user
+    else
+      flash.now[:danger] = t ".invalid"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    flash[:info] = t ".bye"
+    redirect_to root_url
+  end
+end
