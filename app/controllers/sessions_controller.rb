d868cb2e43571c1a473e339de59b8f5a3a593d6e
NguyenMinhTuan2906 <monteeladykillah@gmail.com>
2017-06-29 12:48:20 +0700

    User Manage Profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       flash[:success] = t(".welcome") + user.name + "."
-      redirect_to user
+      redirect_back_or
     else
       flash.now[:danger] = t ".invalid"
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  before_action :load_user, except: [:new, :index, :create]
+  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :prevent_signup, only: [:new, :create]
+  before_action :verify_admin!, only: [:index]
+
+  def index
   end
 
-  def show
-    @user = User.find_by id: params[:id]
+  def new
+    @user = User.new
   end
 
   def create
@@ -18,9 +23,49 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".delete"
+    else
+      flash[:danger] = t ".delete_fail"
+    end
+    redirect_to users_url
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t "none"
+    redirect_to root_url
+  end
+
+  def correct_user
+    return if @user.is_user? current_user
+    flash[:danger] = t "can_not"
+    redirect_to root_url
+  end
+
+  def prevent_signup
+    return unless current_user
+    flash[:danger] = t "prevent_signup"
+    redirect_to root_url
   end
 end
